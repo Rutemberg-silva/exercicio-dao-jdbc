c5a3fc0a683574efded336702b03d82a088d7109
Rutemberg-silva <rutemberg.silva@hotmail.com>
2025-08-09 15:09:07 -0300

    findALL Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,14 +10,20 @@ import java.util.List;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("==== TESTE NUM 1 ====");
+		System.out.println("==== TESTE NUM 1 Find by id ====");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
-		System.out.println("==== TESTE NUM 2 ====");
+		System.out.println("==== TESTE NUM 2 find by department ====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
+		for  (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println("==== TESTE NUM 3 Find all ====");
+		list = sellerDao.findAll();
 		for  (Seller obj : list) {
 			System.out.println(obj);
 		}
