d282d3012470dc7c11797da1856693deaa20c604
Rutemberg-silva <rutemberg.silva@hotmail.com>
2025-08-09 12:13:11 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,6 +9,8 @@ import java.util.Date;
 
 import db.DB;
 import db.DbIntegrityException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -17,6 +19,7 @@ public class Program {
 	public static void main(String[] args) {
 		Department obj1 = new Department(1,"Books");
 		Seller seller = new Seller(21,"Bob", "bob@hotmail.com", new Date(),2500.00,obj1);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(obj1);
 		System.out.println(seller);
 
