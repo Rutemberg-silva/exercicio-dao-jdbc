905c425a90cdd4844f42bc7011ef3f287f84ed18
Rutemberg-silva <rutemberg.silva@hotmail.com>
2025-08-09 14:04:44 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-
+		System.out.println("==== TESTE NUM 1 ====");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
