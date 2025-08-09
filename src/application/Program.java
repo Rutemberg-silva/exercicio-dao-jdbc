6622f4eb984d82a863227fac8cfc659194e5763c
Rutemberg-silva <rutemberg.silva@hotmail.com>
2025-08-09 00:16:11 -0300

    Seller entry Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,16 +3,23 @@ package application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import db.DB;
 import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
 		Department obj1 = new Department(1,"Books");
+		Seller seller = new Seller(21,"Bob", "bob@hotmail.com", new Date(),2500.00,obj1);
 		System.out.println(obj1);
+		System.out.println(seller);
+
+
 	}
 }
